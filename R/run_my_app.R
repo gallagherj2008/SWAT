2887a16d2e6ebcc25b6ea88f7765ef766b6e1407
gallagherj2008 <james.c.gallagher@gmail.com>
2018-02-05 08:59:51 -0500

    added .travis.yml file

---
+++ R/run_my_app.R
@@ -0,0 +1,21 @@
+#' @title Run an app in my package
+#'
+#' @description Use `run_my_app` to start and use apps located within in the package
+#'
+#' @param app_name character string for the package you want to run  
+#' @param ... Additional options pased to shinyAppDir
+#'
+#' @return A printed shiny app
+#' @export
+
+run_my_app <- function(app_name, ...){
+  
+  
+ app_dir <- system.file("apps",app_name, package = "SWAT")
+ 
+ 
+ 
+ shiny::shinyAppDir(appDir = app_dir, options = list(...))
+  
+  
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(SWAT)
+
+test_check("SWAT")
