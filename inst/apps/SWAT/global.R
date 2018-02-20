9cf7ba5e84002f023fdcb8daed8be02f966ed863
gallagherj2008 <james.c.gallagher@gmail.com>
2018-02-20 14:06:28 -0500

    added time component to app; deleted sidebar layout

--- inst/apps/SWAT/global.R
+++ inst/apps/SWAT/global.R
@@ -2,7 +2,9 @@ library(shiny)
 library(tidyverse)
 library(igraph)
 library(Matrix)
+library(lubridate)
 
 data('webclickexample')
 clicks <- webclickexample
 clicks <- as.data.frame(clicks)
+clicks$DURATION_FROMCLICKTOCREATION <-as.numeric((clicks$DURATION_FROMCLICKTOCREATION)/lubridate::dhours(x = 1)) 
--- inst/apps/SWAT/server.R
+++ inst/apps/SWAT/server.R
@@ -10,20 +10,50 @@ server <- function(input, output, session) {
   
   output$barchart <- renderPlot({
     
-    selecteddata <- reactive({
-                    clicks[, c("referrer",
-                             "VENDORNAME_OPERATINGSYSTEM",
-                             "TYPE_HARDWAREPLATFORM")]
+
+  if (input$plotcolumn %in% c("VENDORNAME_OPERATINGSYSTEM", "TYPE_HARDWAREPLATFORM","referrer")) {  
+    
+    selecteddata <- reactive({ 
+      clicks[clicks[,input$plotcolumn] %in% names(which(table(clicks[,input$plotcolumn]) > input$filterlevel)), 
+             c("referrer",
+               "VENDORNAME_OPERATINGSYSTEM",
+               "TYPE_HARDWAREPLATFORM", 
+               "DURATION_FROMCLICKTOCREATION",
+               "TIME_CLICKED")]
     })
+    
     
     
    p <- ggplot2::ggplot(data = selecteddata(), aes_string(x = input$plotcolumn))
    p <- p + ggplot2::geom_bar(aes_string(fill = input$fillcolumn), 
-                        position = "stack")
+                        position = input$chartstyle)
+   p <- p + ggthemes::theme_base()
     print(p)
     
     
+  } else {
+    
+    selecteddata <- reactive({ 
+      clicks[, 
+             c("referrer",
+               "VENDORNAME_OPERATINGSYSTEM",
+               "TYPE_HARDWAREPLATFORM", 
+               "DURATION_FROMCLICKTOCREATION",
+               "TIME_CLICKED")]
+    })
+    
+    
+    p <- ggplot2::ggplot(data = selecteddata(), aes_string(x = input$plotcolumn))
+    p <- p + ggplot2::geom_density(aes_string(group = input$fillcolumn, color = input$fillcolumn))
+    print(p)
+   
+    
+    #stopApp(returnValue = input$plotcolumn)
+     
+  }
   })
+  
+  
   
   
   
