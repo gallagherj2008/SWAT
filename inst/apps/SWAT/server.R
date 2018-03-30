2e10435dd0131eb69192bd95745b73b5b8235a32
whaleshark16 <emilymeyer19@gmail.com>
2018-03-30 14:41:30 -0400

    Updated with additional plots, fixed some errors, and added termstomatch input

--- inst/apps/SWAT/server.R
+++ inst/apps/SWAT/server.R
@@ -9,10 +9,10 @@ server <- function(input, output, session) {
       if (!is.null(input$file1$datapath)) {
       tryCatch({      
       rv$clicks <- SWAT::loadWebClicks(input$file1$datapath)
-      #licks <- as.data.frame(rv$clicks)
-      #stopApp(rv$clicks)
       rv$clicks <- as.data.frame(rv$clicks)
-      rv$clicks <- SWAT::cleanData(rv$clicks, matchvector = termstomatch)
+      rv$termstomatch <- c(unlist(strsplit(input$termstomatchinput,",")))
+      rv$termstomatch1<-gsub("^\\s+|\\s+$", "", rv$termstomatch)
+      rv$clicks <- SWAT::cleanData(rv$clicks, matchvector=rv$termstomatch1)
       #stopApp(rv$clicks)
       shiny::removeModal()
       },
@@ -99,11 +99,31 @@ server <- function(input, output, session) {
     rv$filteredCommunitiesfortables1 <- rv$filteredCommunitiesfortables[1:length(rv$filteredCommunitiesfortables)]
     rv$filteredCommunitiesfortables2 <- rv$filteredCommunitiesfortables1[order(sapply(rv$filteredCommunitiesfortables1,length),decreasing=T)]
     }, priority = 1)
-  
+
+  observe({
+    
+    req(rv$filteredcommunities)
+    
+    rv$filteredCommunitiesforplots2 <- rv$filteredcommunities[[input$Commfilter]]
+    
+    
+    updateSliderInput(session = session, 
+                      "Commstodisplay",
+                      min = 1,
+                      max = length(rv$filteredCommunitiesforplots2),
+                      step = 1)
+    
+    rv$filteredCommunitiesfortables3 <- rv$filteredcommunities[[input$Commfilter]]
+    rv$filteredCommunitiesfortables13 <- rv$filteredCommunitiesfortables3[1:length(rv$filteredCommunitiesfortables3)]
+    rv$filteredCommunitiesfortables23 <- rv$filteredCommunitiesfortables13[order(sapply(rv$filteredCommunitiesfortables13,length),decreasing=T)]
+    rv$dataformeasures<-SWAT::communitiesformeasures(data=rv$clicks,communities=rv$filteredCommunitiesfortables23, commstodisplay=input$Commstodisplay)
+  })
+    
+    
   observeEvent({input$seeTable},{
     
     req(rv$filteredcommunities)
-            #stopApp(rv$commtables)
+            #stopApp(rv$clicks)
       # rv$filteredCommunitiesfortables <- rv$filteredcommunities[[input$commfilter]]
      tryCatch({ 
       
@@ -132,12 +152,10 @@ server <- function(input, output, session) {
   })
 
   output$barchart <- renderPlot({
-    req(input$file1)
+    req(rv$clicks)
     
-    
-
     observe({
-    rv$clicks <- as.data.frame(rv$clicks)
+      rv$clicks <- as.data.frame(rv$clicks)
     })
     
   if (input$plotcolumn %in% c("VENDORNAME_OPERATINGSYSTEM", "TYPE_HARDWAREPLATFORM","referrer")) {
@@ -152,23 +170,19 @@ server <- function(input, output, session) {
                                      "TIME_CLICKED")]
     })
 
-    #rv$selecteddata <- rv$clicks[,c("referrer","VENDORNAME_OPERATINGSYSTEM","DURATION_FROMCLICKTOCREATION","TIME_CLICKED")]
-    #stopApp(rv$selecteddata)
     shiny::req(rv$selecteddata$referrer, cancelOutput = T)
-   p <- ggplot2::ggplot(data = rv$selecteddata, aes_string(x = input$plotcolumn))
-   p <- p + ggplot2::geom_bar(aes_string(fill = input$fillcolumn),
-                        position = input$chartstyle,
-                        show.legend = T) +
-     ggplot2::ylab("Count")
-   p <- p + ggthemes::theme_base()
-    print(p)
+    p <- ggplot2::ggplot(data = rv$selecteddata, aes_string(x = input$plotcolumn))+
+        ggplot2::geom_bar(aes_string(fill = input$fillcolumn),position = input$chartstyle, show.legend = T) +
+        ggthemes::theme_base()+
+        scale_y_continuous(name="Count", labels = scales::comma)
+        
+     print(p)
+     
 
 
   } 
     
-    if (input$plotcolumn %in% c("DURATION_FROMCLICKTOCREATION", "TIME_CLICKED")) {
-    
-    #updateSliderInput(session, "agefilter", max = round(max(rv$selecteddata$DURATION_FROMCLICKTOCREATION),digits = 0), step = 24, min = 1)
+  if (input$plotcolumn %in% c("DURATION_FROMCLICKTOCREATION", "TIME_CLICKED")) {
     
     observe({rv$selecteddata1 <- rv$clicks[rv$clicks[,input$fillcolumn] %in% (names(which(table(rv$clicks[,input$fillcolumn]) > input$filterlevel))),
                                  c("referrer",
@@ -177,36 +191,36 @@ server <- function(input, output, session) {
                                    "DURATION_FROMCLICKTOCREATION",
                                    "TIME_CLICKED")]
     
-    #rv$selecteddata <- rv$selecteddata1[rv$selecteddata1[, input$plotcolumn] < input$agefilter, ]
-    
     })
     
     if (input$plotcolumn == "DURATION_FROMCLICKTOCREATION") {
 
-    observe({
-      #rv$selecteddata2 <- rv$selecteddata1
+     observe({
       suppressWarnings({
       rv$selecteddata2 <- rv$selecteddata1[rv$selecteddata1[,input$plotcolumn] < input$agefilter*24, ]
       })
-    })
-
-      #stopApp(rv$selecteddata$DURATION_FROMCLICKTOCREATION)
+     })
+      
+      
+      shiny::req(rv$selecteddata2$referrer, cancelOutput = T)
+      p <- ggplot2::ggplot(data = rv$selecteddata2, aes_string(x = input$plotcolumn))+
+        ggplot2::geom_density(aes_string(group = input$fillcolumn, color = input$fillcolumn))+
+        ggthemes::theme_base()+
+        ggplot2::xlab("Duration from Click to Creation (Hours)") +
+        ggplot2::ylab("Density") 
+      print(p)
+      
     }
       
     else {
       
-      rv$selecteddata2 <- rv$selecteddata1
+      shiny::req(rv$selecteddata1$referrer, cancelOutput = T)
+      p <- ggplot2::ggplot(data = rv$selecteddata1, aes_string(x = input$plotcolumn))
+      p <- p + ggplot2::geom_density(aes_string(group = input$fillcolumn, color = input$fillcolumn))
+      p <- p + ggthemes::theme_base()
+      print(p)
       
     }
-    
-      
-      shiny::req(rv$selecteddata2$referrer, cancelOutput = T)
-    p <- ggplot2::ggplot(data = rv$selecteddata2, aes_string(x = input$plotcolumn))
-    p <- p + ggplot2::geom_density(aes_string(group = input$fillcolumn, color = input$fillcolumn))
-    p <- p + ggthemes::theme_base()
-    print(p)
-
-
 
 
   }
@@ -316,7 +330,125 @@ server <- function(input, output, session) {
 
   })
   
-  
+
+  output$barchartend <- renderPlot({
+    
+    if (input$plot == "referrer") {
+      
+        
+        q<-  rv$dataformeasures %>% 
+          filter(rv$dataformeasures$referrer != "direct") %>%
+          ggplot2::ggplot() +
+          ggplot2::geom_bar(ggplot2::aes(x= rv$dataformeasures$community, fill =   rv$dataformeasures$referrer),position = input$Chartstyle, na.rm= T) +
+          ggplot2::xlab("Community") +
+          ggplot2::ylab("Number of Clicks") +
+          ggplot2::ggtitle("Total Number of Clicks \nPer Community by referrer type") +
+          ggplot2::guides(fill=guide_legend(title=NULL)) +
+          ggplot2::theme_minimal( base_size = 24)+
+          ggplot2::theme(plot.title = element_text(hjust = 0.5))
+        
+        print(q)
+      
+      
+    } 
+    
+    if (input$plot == "TYPE_HARDWAREPLATFORM") {
+      
+      rv$dataformeasures4<-rv$dataformeasures %>%
+        filter(rv$dataformeasures$TYPE_HARDWAREPLATFORM != "")
+      
+      q<-rv$dataformeasures4 %>%
+        ggplot2::ggplot() +
+        ggplot2::geom_bar(ggplot2::aes(x=rv$dataformeasures4$community, fill = rv$dataformeasures4$TYPE_HARDWAREPLATFORM),position = input$Chartstyle, na.rm= T) +
+        ggplot2::xlab("Community") +
+        ggplot2::ylab("Number of Clicks") +
+        ggplot2::ggtitle("Links by \nHardware Platform in Each Community") +
+        ggplot2::guides(fill=guide_legend(title=NULL)) +
+        ggplot2::theme_minimal( base_size = 24) +
+        ggplot2::theme(plot.title = element_text(hjust = 0.5))
+      
+      print(q)
+      
+      
+      }
+      
+      if (input$plot ==  "TIME_CLICKED") {
+        
+        q<- ggplot2::ggplot(data= rv$dataformeasures, ggplot2::aes(x= rv$dataformeasures$TIME_CLICKED,group=rv$dataformeasures$community, color = rv$dataformeasures$community )) + 
+          ggplot2::geom_density(size = 1.3) +
+          ggplot2::ggtitle("Distribution of Time of Day \nof Each Click by Community") + 
+          ggplot2::xlab("Time Clicked") +
+          ggplot2::ylab("Density") +
+          ggplot2::guides(fill=guide_legend(title=NULL)) +
+          ggplot2::theme_minimal(base_size = 24)+
+          ggplot2::theme(plot.title = element_text(hjust = 0.5))+
+          ggplot2::theme(legend.title=element_blank())
+        
+        print(q)
+      }
+      
+     if (input$plot ==  "VENDORNAME_OPERATINGSYSTEM"){
+       
+       rv$dataformeasures3<-rv$dataformeasures %>% 
+         filter(rv$dataformeasures$VENDORNAME_OPERATINGSYSTEM != "Canonical Ltd.")
+       rv$dataformeasures5<-rv$dataformeasures3 %>% 
+         filter(rv$dataformeasures3$VENDORNAME_OPERATINGSYSTEM != "Nintendo of America Inc.")
+       rv$dataformeasures6<-rv$dataformeasures5 %>% 
+         filter(rv$dataformeasures5$VENDORNAME_OPERATINGSYSTEM != "Sony Computer Entertainment")
+       
+        q<-rv$dataformeasures6 %>% 
+          ggplot2::ggplot() +
+          ggplot2::geom_bar(ggplot2::aes(x=rv$dataformeasures6$community, fill = rv$dataformeasures6$VENDORNAME_OPERATINGSYSTEM),position = input$Chartstyle, na.rm= T) +
+          ggplot2::xlab("Community") +
+          ggplot2::ylab("Number of Clicks") +
+          ggplot2::ggtitle("Links by Operating System \nin Each Community") +
+          ggplot2::guides(fill=guide_legend(title=NULL)) +
+          ggplot2::theme_minimal( base_size = 24) +
+          ggplot2::theme(plot.title = element_text(hjust = 0.5))
+        
+        print(q)
+        
+     }
+    
+    if (input$plot ==  "DURATION_FROMCLICKTOCREATION") {
+      
+      rv$dataformeasures4<-rv$dataformeasures %>% 
+        filter(rv$dataformeasures$DURATION_FROMCLICKTOCREATION < input$Agefilter*24)
+      
+      q<-ggplot(data= rv$dataformeasures4, ggplot2::aes(x= rv$dataformeasures4$DURATION_FROMCLICKTOCREATION,group=rv$dataformeasures4$community, color = rv$dataformeasures4$community )) + 
+        ggplot2::geom_density(size = 1.3) +
+        ggplot2::ggtitle("Distribution of Duration from Click to Creation \nby Community") + 
+        ggplot2::xlab("Duration from Click to Creation") +
+        ggplot2::ylab("Density") +
+        ggplot2::guides(fill=guide_legend(title=NULL)) +
+        ggplot2::theme_minimal(base_size = 24) +
+        ggplot2::theme(plot.title = element_text(hjust = 0.5))+
+        ggplot2::theme(legend.title=element_blank())
+      
+      print(q)
+    }
+    
+    
+    if (input$plot == "Topic") {
+      
+      
+      q<-  rv$dataformeasures %>% 
+        filter(rv$dataformeasures$referrer != "direct") %>%
+        ggplot2::ggplot() +
+        ggplot2::geom_bar(ggplot2::aes(x= rv$dataformeasures$community, fill =   rv$dataformeasures$topicrelated),position = input$Chartstyle, na.rm= T) +
+        ggplot2::xlab("Community") +
+        ggplot2::ylab("Number of Clicks") +
+        ggplot2::ggtitle("Number of Topic-Related Clicks \nPer Community") +
+        ggplot2::guides(fill=guide_legend(title=NULL)) +
+        ggplot2::theme_minimal( base_size = 24)+
+        ggplot2::theme(plot.title = element_text(hjust = 0.5))
+      
+      print(q)
+      
+      
+    }
+      
+  })
   
   }
   
